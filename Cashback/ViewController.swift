a5da79dd7bb153f60636bd7624c890dd9f72a425
Bidisha <bbiswas@unomaha.edu>
2020-11-10 21:51:21 -0600

    Initial setup

---
+++ Cashback/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Cashback
+//
+//  Created by Bidisha Biswas on 11/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "New screen"
+        view.backgroundColor = .systemPink
+    }
+}
+
