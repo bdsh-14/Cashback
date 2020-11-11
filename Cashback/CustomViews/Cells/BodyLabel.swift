9c9e338c5609903553f152187d957f5e8832cc48
Bidisha <bbiswas@unomaha.edu>
2020-11-11 14:31:37 -0600

    create new label type and caching images

---
+++ Cashback/CustomViews/Cells/BodyLabel.swift
@@ -0,0 +1,20 @@
+//
+//  BodyLabel.swift
+//  Cashback
+//
+//  Created by Bidisha Biswas on 11/11/20.
+//
+
+import UIKit
+
+class BodyLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
